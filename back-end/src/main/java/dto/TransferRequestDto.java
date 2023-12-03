4b7238ea6b6a9344d6b7d84da2b6cb0c20cf52e5
ArjunS132 <arjunsgill132@gmail.com>
2023-12-03 00:22:40 -0800

    expanded TransferRequestDto to include the state of a request and include the usernames for parties involved

--- back-end/src/main/java/dto/TransferRequestDto.java
+++ back-end/src/main/java/dto/TransferRequestDto.java
@@ -5,10 +5,21 @@ import org.bson.Document;
 public class TransferRequestDto extends BaseDto {
 
     public double amount;
-    public String fromId;
+    public String fromUserName;
+	public String fromId;
+    public String toUserName;
     public String toId;
+    public RequestStatus status;
 
-    public TransferRequestDto() {
+    public String getToId() {
+		return toId;
+	}
+
+	public void setToId(String toId) {
+		this.toId = toId;
+	}
+
+	public TransferRequestDto() {
         super();
     }
 
@@ -16,25 +27,59 @@ public class TransferRequestDto extends BaseDto {
         super(uniqueId);
     }
 
-    public TransferRequestDto(double amount, String fromId, String toId) {
-        this.amount = amount;
-        this.fromId = fromId;
-        this.toId = toId;
+    public String getFromId() {
+        return this.fromId;
+    }
+
+    public String getToUserName() {
+        return this.toUserName;
+    }
+
+    public double getAmount() {
+        return this.amount;
+    }
+
+    public void setAmount(double amount) {
+		this.amount = amount;
+	}
+
+	public void setFromId(String fromId) {
+		this.fromId = fromId;
+	}
+
+	public void setToUserName(String toId) {
+		this.toUserName = toId;
+	}
+
+    public String getFromUserName() {
+		return fromUserName;
+	}
+
+	public void setFromUserName(String fromUserName) {
+		this.fromUserName = fromUserName;
+	}
+
+	public RequestStatus getStatus() {
+		return status;
+	}
+
+	public void setStatus(RequestStatus state) {
+		this.status = state;
+	}
+
+	public Document toDocument() {
+        return new Document()
+                .append("amount", this.amount)
+                .append("fromId", this.fromId)
+                .append("toId", this.toUserName);
     }
 
     public static TransferRequestDto fromDocument(Document match) {
         TransferRequestDto transferRequestDto = new TransferRequestDto();
         transferRequestDto.amount = match.getDouble("amount");
         transferRequestDto.fromId = match.getString("fromId");
+        transferRequestDto.toUserName = match.getString("toUserName");
         transferRequestDto.toId = match.getString("toId");
         return transferRequestDto;
     }
-
-    public Document toDocument() {
-        return new Document()
-                .append("amount", this.amount)
-                .append("fromId", this.fromId)
-                .append("toId", this.toId);
-    }
-
 }
